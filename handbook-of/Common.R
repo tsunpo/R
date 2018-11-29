007ac8add69d28b4328f0c7fa465adb8ccbb0646
tsunpo <tyang2@uni-koeln.de>
2018-11-29 14:58:14 +0100

    subset0()

--- handbook-of/Common.R
+++ handbook-of/Common.R
@@ -11,7 +11,7 @@
 #         2) Return a vector if only one column is in the file.
 # Usages: table <- readTable("file.txt", header=T, rownames=T, sep="\t")
 #         table <- readTable("file.txt", header=T, rownames=2, sep="\t")
-#         list  <- readTable("list.txt", header=F, rownames=F, sep="")
+#         array <- readTable("list.txt", header=F, rownames=F, sep="")
 # Last Modified: 22/10/16
 # -----------------------------------------------------------------------------
 readTable <- function(file, header, rownames, sep) {
@@ -63,21 +63,13 @@ toTable <- function(value, ncol, nrow, colnames) {
 }
 
 # -----------------------------------------------------------------------------
-# Method: Modification of subset(refGene, name2 == "MYCL")
-# Usages: subset0(refGene, "name2", "MYCL")
-#         subset0(refGene, "name2", c("MYCL", "MYCN", "MYC"))   ## BUG FIX 16/02/17
-# Last Modified: 16/02/17
+# Method: Modification of subset(ensGene, external_gene_name %in% "MYCL")
+# Usages: subset0(ensGene, "external_gene_name", "MYCL")
+#         subset0(ensGene, "external_gene_name", c("MYC", "MYCL", "MYCN"))
+# Last Modified: 29/11/18
 # -----------------------------------------------------------------------------
-subset0 <- function(df, filter, value) {
-   if (length(value) == 1) {
-      return(df[which(value == df[,filter]),])   ## BUG FIX 16/02/17: Dose not work on length(value) != 1
-   } else {
-      subsets <- df[which(value[1] == df[,filter]),]
-      for (v in 2:length(value))
-         subsets <- rbind(subsets, df[which(value[v] == df[,filter]),])
-      
-      return(subsets)
-   }
+subset0 <- function(df, colname, values) {
+   return(df[which(df[, colname] %in% values),])   ## NEW VERSION: 29/11/18
 }
 
 separator <- function(number) {
